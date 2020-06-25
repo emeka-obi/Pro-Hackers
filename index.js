d8ea00b029f74bd78bcd8c319c034a743880c856
Andrew Chen <chen901@usc.edu>
2020-06-25 12:34:34 -0600

    testing dialogflow data

--- index.js
+++ index.js
@@ -13,6 +13,16 @@ app.get('/', (req, res) => {
   res.send('TESTING 123')
 })
 
+app.post('/getdata', (req, res) => {
+  // Check that data exists, set variable dataFound to the data if it does
+  const dataFound = req.body.queryResult && req.body.queryResult.parameters && req.body.queryResult.parameters.data
+  ? req.body.result.parameters.restaurant : ' '
+  res.json({
+    fulfillmentText: dataFound,
+    source: 'getdata'
+  })
+})
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
