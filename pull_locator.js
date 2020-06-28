a499372da86bf7f640fe9d98f81d181482e90d95
Sahil Vaidya <sahilvaidya99@gmail.com>
2020-06-27 21:34:10 -0500

    fixed api calls

--- pull_locator.js
+++ pull_locator.js
@@ -1,36 +1,36 @@
-var https = require('follow-redirects').https;
-var fs = require('fs');
+// var request = require('request');
+// var req = request.defaults();
+// var fs = require('fs');
 
+// req.post({
+//     uri : "https://sandbox.api.visa.com/",
+
+//     headers: {
+//       'Content-Type' : 'application/json',
+//       'Accept' : 'application/json',
+//       'Authorization' : 'Basic ' + new Buffer(userId + ':' + password).toString('base64')
+//     },
+//     body: data
+//   }, function(error, response, body) {
+//   }
+// );
+
+var request = require('request');
+var fs = require('fs')
 var options = {
   'method': 'POST',
-  'hostname': 'sandbox.api.visa.com',
-  'path': '/merchantlocator/v1/locator',
+  'url': 'https://sandbox.api.visa.com/visaqueueinsights/v1/queueinsights',
+  'key': fs.readFileSync("key_2023928c-0c6a-4108-b260-d99709d8bd62.pem"),
+  'cert': fs.readFileSync("cert.pem"),
   'headers': {
-    'Authorization': 'Basic NFdNMVFCRFQ2Tk1QU0c0MEk3QjgyMWZTUzBsVUc0X2ZIdmZ1akdjc0ZKQ3RHWDY0MDo0YlZLcTV3MzE3TTNsNlo3QU81dFY=',
+    'Accept': 'application/json',
+    'Authorization': 'Basic RFRPSVRBTFdWV1pXRFE2U1FNMTAyMXl5eHB5T1R2RmYxR3BXenIwRmNxTWN6YjMxTTpaczVCNzBVSDEz:',
     'Content-Type': 'application/json'
   },
-  'maxRedirects': 20
+  body: JSON.stringify({"requestHeader":{"messageDateTime":"2020-06-26T19:09:18.327","requestMessageId":"6da60e1b8b024532a2e0eacb1af58581"},"requestData":{"kind":"predict"}})
+
 };
-
-var req = https.request(options, function (res) {
-  var chunks = [];
-
-  res.on("data", function (chunk) {
-    chunks.push(chunk);
-  });
-
-  res.on("end", function (chunk) {
-    var body = Buffer.concat(chunks);
-    console.log(body.toString());
-  });
-
-  res.on("error", function (error) {
-    console.error(error);
-  });
+request(options, function (error, response) {
+  if (error) throw new Error(error);
+  console.log(response.body);
 });
-
-var postData = JSON.stringify({"header":{"messageDateTime":"2020-06-25T14:39:55.903","requestMessageId":"Request_001","startIndex":"0"},"searchAttrList":{"merchantName":"Starbucks","merchantCountryCode":"840","latitude":"37.363922","longitude":"-121.929163","distance":"2","distanceUnit":"M"},"responseAttrList":["GNLOCATOR"],"searchOptions":{"maxRecords":"5","matchIndicators":"true","matchScore":"true"}});
-
-req.write(postData);
-
-req.end();
