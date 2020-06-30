06e18ba69cf1890132584691507fcd644bb17538
Tyler Amano-Smerling <amanosme@usc.edu>
2020-06-29 23:10:33 -0700

    ok, back to normal

--- index.js
+++ index.js
@@ -8,6 +8,56 @@ const fs = require('fs')
 const app = express()
 app.set('views', path.join(__dirname, 'views'))
 
+/*app.post('/getdata', (req, res) => {
+    var responseText = '';
+    var restName = "", restLoc, keyWord;
+    for (const context of req.body.queryResult.outputContexts) {
+        if (context.name.includes("zip") && !context.name.includes("followup")) {
+            //Parse out restaurant name and location
+            var tempName = context.parameters.restaurant;
+            var tempLoc = context.parameters.address;
+            var zip = context.parameters.zip;
+            if (!tempLoc.includes(zip)) tempLoc += "-" + zip;
+            //Remove whitespaces and commas
+            if (tempName.length != 0) {
+                restName = tempName.split(' ').join('-');
+            } 
+            restLoc = tempLoc.split(' ').join('-');
+            
+            break;
+        }
+    }
+    //Option 1: specific restaurant
+    if (req.body.queryResult.intent.displayName.includes("list-options - 1 - checkapi")) {
+    }
+    //Option 2: multiple restaurants, sort by wait time
+    else if (req.body.queryResult.intent.displayName.includes("list-options - 2 - checkapi")) {
+    }
+    //Search yelp
+    //var searchUrl = "https://api.yelp.com/v3/businesses/search?term=" + restName + "&location=" + restLoc;
+    responseText = "https://api.yelp.com/v3/businesses/search?term=" + restName + "&location=" + restLoc;
+    var searchUrl = "https://api.yelp.com/v3/businesses/search?term=ice-cream&latitude=33.835850&longitude=-118.366150"
+//     var config = {
+//         method: 'get',
+//         url: searchUrl,
+//         headers: {
+//             'Authorization': 'Bearer qxzauzGWC0i9v6BEJGzkV7kRCUBZE1FWJB16OGgn-XB-DdKIRuk-_4RFjNhJSbvD6VhttsdAMNU_broBe1ZpqgOLeqdyS7o9HXPz_bMZHyLOw6nxd4TmAQ37ZCD5XnYx'
+//         }
+//     };
+    
+    var myPath = '/v3/businesses/search?term=' + restName + "&location=" + restLoc;
+    var options = {
+        'method': 'GET',
+        'hostname': 'api.yelp.com',
+        'path': myPath,
+        'headers': {
+        'Authorization': 'Bearer qxzauzGWC0i9v6BEJGzkV7kRCUBZE1FWJB16OGgn-XB-DdKIRuk-_4RFjNhJSbvD6VhttsdAMNU_broBe1ZpqgOLeqdyS7o9HXPz_bMZHyLOw6nxd4TmAQ37ZCD5XnYx'
+  },
+  'maxRedirects': 20
+};*/
+
+
+
 app.post('/getmerchant', (req, res) => {
     var zip = 90007
     var options = {
@@ -29,6 +79,23 @@ app.post('/getmerchant', (req, res) => {
   
       apiResponse.on("data", function (chunk) {
         chunks.push(chunk);
+      });
+  
+      apiResponse.on("end", function (chunk) {
+        var body = Buffer.concat(chunks);
+        var jsonRes = JSON.parse(body);
+        var restaurantRes = jsonRes.merchantLocatorServiceResponse.response;
+        var restaurantNames = [];
+        //console.log(body.toString());
+      //  Below loop prints out each restaurant name
+      // to see all parameters change to restaurantRes[i]
+        for (var i = 0; i < restaurantRes.length; ++i) {
+          console.log(restaurantRes[i].responseValues.visaStoreName)
+          restaurantNames.push(restaurantRes[i].responseValues.visaStoreName)
+        }
+        res.json ({
+          fulfillmentText: restaurantNames,
+          location: zip
         })
       });
   
@@ -49,7 +116,7 @@ app.post('/getmerchant', (req, res) => {
   apiRequest.end();
   })
   
-
+  
   const PORT = process.env.PORT || 3000
-
-  app.listen(PORT, () => console.log(`Server running on port ${PORT}`)) 
+  
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
