5d0ba2c98863fee32eb7981b01102a0194751c0b
Tyler Amano-Smerling <amanosme@usc.edu>
2020-06-30 12:51:41 -0700

    testing yelp search

--- index.js
+++ index.js
@@ -42,8 +42,8 @@ app.post('/getmerchant', (req, res) => {
     }*/
 
     //yelp search
-    /* var myPath = '/v3/businesses/search?term=' + restName + "&location=" + restLoc;
-    var options = {
+    var myPath = '/v3/businesses/search?term=' + restName + "&location=" + restLoc;
+    var yelpOptions = {
         'method': 'GET',
         'hostname': 'api.yelp.com',
         'path': myPath,
@@ -51,10 +51,35 @@ app.post('/getmerchant', (req, res) => {
         'Authorization': 'Bearer qxzauzGWC0i9v6BEJGzkV7kRCUBZE1FWJB16OGgn-XB-DdKIRuk-_4RFjNhJSbvD6VhttsdAMNU_broBe1ZpqgOLeqdyS7o9HXPz_bMZHyLOw6nxd4TmAQ37ZCD5XnYx'
         },
         'maxRedirects': 20
-    };*/
-    //call Visa merchant locator API
+    };
+    
+    var yelpRequest = https.request(options, function (res) {
+    var chunks = [];
+
+      res.on("data", function (chunk) {
+        chunks.push(chunk);
+      });
+
+      res.on("end", function (chunk) 
+        var yelpBody = Buffer.concat(chunks);
+        var yelpRes = JSON.parse(body);
+        responseText += yelpRes.businesses[0].name;
+         
+        res.json ({
+          fulfillmentText: responseText,
+          source: 'getmerchant'
+        })
+      });
+
+      res.on("error", function (error) {
+        console.error(error);
+      });
+    });
+
+    req.end();
+
     //var zip = 90007
-    var options = {
+    var visaOptions = {
       'method': 'POST',
       'hostname': 'sandbox.api.visa.com',
       'key': fs.readFileSync("key_ad1d3f5e-0d23-4610-9a28-6aca251871b0.pem"),
@@ -67,52 +92,51 @@ app.post('/getmerchant', (req, res) => {
       },
       'maxRedirects': 20
     };
+//     var apiRequest = https.request(visaOptions, function (apiResponse) {
+//       var chunks = [];
 
-    var apiRequest = https.request(options, function (apiResponse) {
-      var chunks = [];
+//       apiResponse.on("data", function (chunk) {
+//         chunks.push(chunk);
+//       });
 
-      apiResponse.on("data", function (chunk) {
-        chunks.push(chunk);
-      });
+//       apiResponse.on("end", function (chunk) {
+//         var body = Buffer.concat(chunks);
+//         var jsonRes = JSON.parse(body);
+//         var restaurantRes = jsonRes.merchantLocatorServiceResponse.response;
+//         var restaurantNames = [];
+//         //console.log(body.toString());
+//       //  Below loop prints out each restaurant name
+//       // to see all parameters change to restaurantRes[i]
+//         for (var i = 0; i < restaurantRes.length; ++i) {
+//           console.log(restaurantRes[i].responseValues.visaStoreName)
+//           restaurantNames.push(restaurantRes[i].responseValues.visaStoreName)
+//         }
+//         res.json ({
+//           fulfillmentText: restaurantNames,
+//           location: zip,
+//           requestedRestaurant: tempName,
+//           addressLocation: restLoc
+//         })
+//       });
 
-      apiResponse.on("end", function (chunk) {
-        var body = Buffer.concat(chunks);
-        var jsonRes = JSON.parse(body);
-        var restaurantRes = jsonRes.merchantLocatorServiceResponse.response;
-        var restaurantNames = [];
-        //console.log(body.toString());
-      //  Below loop prints out each restaurant name
-      // to see all parameters change to restaurantRes[i]
-        for (var i = 0; i < restaurantRes.length; ++i) {
-          console.log(restaurantRes[i].responseValues.visaStoreName)
-          restaurantNames.push(restaurantRes[i].responseValues.visaStoreName)
-        }
-        res.json ({
-          fulfillmentText: restaurantNames,
-          location: zip,
-          requestedRestaurant: tempName,
-          addressLocation: restLoc
-        })
-      });
+//       apiResponse.on("error", function (error) {
+//         console.error(error);
+//         res.json ({
+//           fulfillmentText: "No restaurants found nearby",
+//           location: zip,
+//           requestedRestaurant: tempName,
+//           addressLocation: restLoc
+//         })
+//       });
+//   })
 
-      apiResponse.on("error", function (error) {
-        console.error(error);
-        res.json ({
-          fulfillmentText: "No restaurants found nearby",
-          location: zip,
-          requestedRestaurant: tempName,
-          addressLocation: restLoc
-        })
-      });
-  })
+//   var postData = JSON.stringify({"header":{"messageDateTime":"2020-06-26T19:08:07.903","requestMessageId":"Request_001","startIndex":"0"},
+//   "searchAttrList":{"merchantCategoryCode":["5812","5814"],"merchantCountryCode":"840","merchantPostalCode":zip,"distance":"2","distanceUnit":"M"},
+//   "responseAttrList":["GNLOCATOR"],"searchOptions":{"maxRecords":"5","matchIndicators":"true","matchScore":"true"}})
+//   apiRequest.write(postData);
 
-  var postData = JSON.stringify({"header":{"messageDateTime":"2020-06-26T19:08:07.903","requestMessageId":"Request_001","startIndex":"0"},
-  "searchAttrList":{"merchantCategoryCode":["5812","5814"],"merchantCountryCode":"840","merchantPostalCode":zip,"distance":"2","distanceUnit":"M"},
-  "responseAttrList":["GNLOCATOR"],"searchOptions":{"maxRecords":"5","matchIndicators":"true","matchScore":"true"}})
-  apiRequest.write(postData);
-
-  apiRequest.end();
-  })
+//   apiRequest.end();
+//   })
 
 
   const PORT = process.env.PORT || 3000
